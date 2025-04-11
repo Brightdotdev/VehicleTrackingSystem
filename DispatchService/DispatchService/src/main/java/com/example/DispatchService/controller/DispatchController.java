0c802b8e4f67cbe97685f90d4fc522796ca5a8f4
Brightdotev <iamtherealbright@gmail.com>
2025-04-11 23:25:10 +0100

    initial commit

--- DispatchService/DispatchService/src/main/java/com/example/DispatchService/controller/DispatchController.java
+++ DispatchService/DispatchService/src/main/java/com/example/DispatchService/controller/DispatchController.java
@@ -1,2 +1,17 @@
-package com.example.DispatchService.controller;public class DispatchController {
+package com.example.DispatchService.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/dispatch")
+public class DispatchController {
+
+
+    @GetMapping
+    public String getOne() {
+        return "Dispatch ooo";
+    }
 }
--- UserService/UserService/src/main/java/com/example/UserService/controller/UserController.java
+++ UserService/UserService/src/main/java/com/example/UserService/controller/UserController.java
@@ -1,2 +1,19 @@
-package com.example.UserService.controller;public class UserController {
+package com.example.UserService.controller;
+
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+
+    @GetMapping
+    public String testMethod(){
+        return  "Users oooo";
+    }
+
 }
