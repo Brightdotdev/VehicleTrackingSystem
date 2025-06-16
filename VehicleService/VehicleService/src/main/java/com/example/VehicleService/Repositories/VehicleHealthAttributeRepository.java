0707128c33d87d14ae2ce938e73751c6677a27e0
Brightdotev <iamtherealbright@gmail.com>
2025-06-16 02:44:53 +0100

    Extra configurations

--- VehicleService/VehicleService/src/main/java/com/example/VehicleService/Repositories/VehicleHealthAttributeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.VehicleService.Repositories;
-
-import com.example.VehicleService.Models.VehicleHealthAttributeModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VehicleHealthAttributeRepository  extends JpaRepository<VehicleHealthAttributeModel, Long> {
-
-}
--- logging-tracking-service(G.V.1)/logging-tracking-service/src/main/java/com/tracker/loggingtrackingservice/G/V1/Exceptions/NotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.tracker.loggingtrackingservice.G.V1.Exceptions;
-
-public class NotFoundException extends  RuntimeException{
-
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
