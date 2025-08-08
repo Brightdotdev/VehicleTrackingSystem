0622ba147a530f404fafe74301659e1dbec3aa84
Brightdotev <iamtherealbright@gmail.com>
2025-08-08 02:17:08 +0100

    update :: extra security configurtion tweaks

--- views/admin_desk_web/src/app/page.tsx
+++ views/admin_desk_web/src/app/page.tsx
@@ -1,11 +1,10 @@
 "use client"
 
 import Loading from '@/components/ui/Loading';
+import UnvalidatedPage from '@/components/UnvalidatedPage';
 import { useUserValidation } from '@/hooks/useUserValidation';
 import dynamic from 'next/dynamic';
-import { useSearchParams } from 'next/navigation';
 import React, { useEffect } from 'react';
-import { toast } from 'sonner';
 
 const AdminHomePage = dynamic(() => import('@/components/ComponentBlocks/AdminHomePage'), {
   loading: () => <Loading />,
@@ -19,22 +18,18 @@ const AdminWelcomePage = dynamic(() => import('@/components/ComponentBlocks/Admi
 
 export default function Page() {
   const { isValidated, loading, checkValidation ,adminDetails } = useUserValidation();
-  const searchParams = useSearchParams();
-  const redirected = searchParams.get("redirected");
-
-  useEffect(() => {
-      checkValidation();
-    console.log(isValidated)
-
-    console.log(loading)
-    if (redirected === "already-logged-in") {
-      toast.error(`You're already logged in`);
-    }
-  }, [redirected]);
-
-  // Handle auth state
-  if (loading && isValidated == null) return <>Validating...</>;
+ 
   
-  if (!isValidated) return <AdminWelcomePage />;
-  if (isValidated) return <AdminHomePage />;
+
+useEffect(() => {
+  checkValidation();
+}, []);
+
+    
+if (loading) return <Loading />;
+
+if (!adminDetails?.username || !adminDetails.licence || !adminDetails.licenceExp) return <UnvalidatedPage />;
+
+return isValidated ? <AdminHomePage /> : <AdminWelcomePage />;
+
 }
