f1231c14f0a11f3d1ed7e9a6ef833df1d7a9a234
Brightdotev <iamtherealbright@gmail.com>
2025-08-11 04:18:14 +0100

    docker compose integration and ease of use for intallation and runing the application lcoally

--- db/init-db.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# Run as the "postgres" user inside the container
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE DATABASE "VEHICLE_AUTH_DB";
-    CREATE DATABASE "VEHICLE_DISPATCH_DB";
-    CREATE DATABASE "VEHICLE_VEHICLE_DB";
-EOSQL
